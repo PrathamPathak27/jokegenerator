c978b66a57b53754afd1d9733a52acfd1c231854
PrathamPathak27 <145742451+PrathamPathak27@users.noreply.github.com>
2023-09-22 10:55:57 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,328 @@
+let myjokes = [
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 23,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Two SQL tables sit at the bar. A query approaches and asks \"Can I join you?\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 221,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 37,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 184,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Your mama's so FAT she can't save files bigger than 4GB.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": true
+    },
+    "id": 9,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A programmer puts two glasses on his bedside table before going to sleep.\nA full one, in case he gets thirsty, and an empty one, in case he doesn't.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 3,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 42,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Knock knock.\nWho's there?\nRecursion.\nRecursion who?\nKnock knock.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 180,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 259,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 57,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says \"Can't you see the warning on the cigarette pack? Smoking is hazardous to your health!\" to which the man replies, \"I am a programmer.  We don't worry about warnings; we only worry about errors.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 38,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I was reading a great book about an immortal dog the other day. It was impossible to put down.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 236,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 22,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 23,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"We messed up the keming again guys.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 20,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 35,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Yo mama is so old, she knew Burger King while he was still a prince.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "safe": true,
+    "id": 306,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "I don't know where I stand on abortion. I like killing babies, but I don't like giving women a choice.",
+    "flags": {
+      "nsfw": true,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": true,
+      "explicit": true
+    },
+    "id": 228,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A programmer puts two glasses on his bedside table before going to sleep.\nA full one, in case he gets thirsty, and an empty one, in case he doesn't.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 3,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I was struggling to figure out how lightning works, but then it struck me.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 220,
+    "safe": true,
+    "lang": "en"
+  }
+]
+
+function get() {
+  let index = Math.floor(Math.random() * (myjokes.length - 1));
+  joke.innerHTML = myjokes[index].joke;
+}
+bhosda.addEventListener('click', get);
